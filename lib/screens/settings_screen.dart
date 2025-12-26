f1b5c7a1e9abc2a550cc15f4bed580d642c75e89
PROTIK MONDAL <dev@protik.eu.org>
2025-12-26 21:49:28 +0600

    fix: Add missing dart:convert import to SettingsScreen

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'dart:convert';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
